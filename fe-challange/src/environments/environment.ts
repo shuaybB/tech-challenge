c13f439043f5da5e24fba0328b25a6f12403b22b
shuayb <badoolas@sheridancollege.ca>
2022-01-14 23:29:04 -0500

    added api key and tokens to env file

--- fe-challange/src/environments/environment.ts
+++ fe-challange/src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  BASE_URL : 'https://7ktgzkl5ce.execute-api.us-east-1.amazonaws.com/prod/',
+  API_KEY: 'AJalp94dfG14ju70pLJhzD0Hzpp7oOZ5egTmTEC1',
+  ADMIN: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyVHlwZSI6IkFETUlOIn0.hR8qytQy9l75YMCW9k9wDYOVJk4qG-qyHC7jqX0I0Ig',
+  FOREIGN_COSTUMER: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyVHlwZSI6IkZPUkVJR04ifQ.iTlQm0tUOJ4Boz72mOtsu3MXOgu7BOtR2dOG6xGSy00',
+  DOMESTIC_COSTUMER: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyVHlwZSI6IkRPTUVTVElDIn0.OdCKqt8b3WpWe5mPt5GwdxapSilObJv9p_JyxZwW17w'
 };
 
 /*
